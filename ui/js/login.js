9276e4049dbf76b02457626ea15c5c9b61c5d3a6
shemaM <azertshema@gmail.com>
2020-11-06 15:47:38 +0200

    ch(index):edited all my pages

--- ui/js/login.js
+++ ui/js/login.js
@@ -1,4 +1,3 @@
-const form = document.getElementById("login-form");
 const inputs = document.querySelectorAll(".input");
 
 function addcl() {
@@ -13,48 +12,61 @@ function remcl() {
   }
 }
 
+console.log("hello");
 inputs.forEach((input) => {
   input.addEventListener("focus", addcl);
   input.addEventListener("blur", remcl);
 });
 
-// show input error
+//  form validation
 
+const form = document.getElementById("login-form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+//  show error
 const showError = (input, message) => {
   const formControl = input.parentElement;
-  formControl.className = "input-div error";
+  formControl.className = "input-field error";
   const small = formControl.querySelector("small");
   small.innerText = message;
 };
 
-//  show input success
-
+//  success
 const showSuccess = (input) => {
   const formControl = input.parentElement;
-  formControl.className = "input-div success";
+  formControl.className = "input-field success";
 };
 
-//  checking required parameter
-
-const checkRequired = (inputArry) => {
-  inputArry.forEach((input) => {
-    console.log(input);
-    // if (input.value.trim() === "") {
-    //   showError(input, `${input} is required`);
-    // } else {
-    //   showSuccess(input);
-    // }
-  });
+// checking email
+const checkEmail = (input) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(input.value.trim())) {
+    showError(input, "Email is not Valid");
+  } else {
+    showSuccess(input);
+  }
+};
+// capitalise firt letter of the error
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 };
 
-// check email validation
+// checking password length
 
-// const checkEmail = (input) => {
-//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-// };
+const checkLength = (input, min) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be atleast ${min} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
 
-// event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  checkRequired([inputs]);
+  checkEmail(email);
+  checkLength(password, 10);
 });
--- ui/js/main.js
+++ ui/js/main.js
@@ -1,16 +1,25 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const oneLink = document.querySelectorAll(".item");
+// const form = document.getElementById("contact-form");
+// const name = document.getElementById("name");
+// const email = document.getElementById("email");
+// const message = document.getElementById("message");
+
+const form = document.querySelectorAll(".index_form");
+const name = document.querySelectorAll("#name");
+const email = document.querySelectorAll("#email");
+const message = document.getElementById("message");
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 
-oneLink.forEach(link=>{
-  link.addEventListener('click',(e)=>{
-  navbarLinks.classList.toggle("active");
-})
-})
+oneLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    navbarLinks.classList.toggle("active");
+  });
+});
 
 // project page css
 
@@ -47,13 +56,69 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-
 // popup form for subscribe
 
-document.getElementById('subscribe_btn').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+document.getElementById("subscribe_btn").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "flex";
 });
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".bg-modal").style.display = "none";
+});
+
+// Show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "index_form error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+};
+
+// Show success message
+const showSuccess = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = "index_form success";
+};
+
+// check email is valid
+const checkEmail = (input) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not Valid");
+  }
+};
+
+// cappitalise the first letter of the id
+
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
+// Check input length
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be atleast ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
+
+// Event listerners
+form.forEach((f) => {
+  f.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkLength(f.name, 3, 40);
+    checkLength(message, 6, 10000);
+    checkEmail(f.email);
+  });
 });
