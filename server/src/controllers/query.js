5157a582ebfca471479af50a2339752e89c1e367
shemaM <azertshema@gmail.com>
2020-12-08 11:24:47 +0200

    ft(query):reading all queries

--- server/src/controllers/query.js
+++ server/src/controllers/query.js
@@ -5,6 +5,13 @@ class Query {
     const query = await Queries.create(req.body);
     return res.status(200).json({ success: true, data: query });
   }
+
+  static async getAll(req, res, next) {
+    const query = await Queries.find();
+    return res
+      .status(200)
+      .json({ success: true, count: query.length, data: query });
+  }
 }
 
 export default Query;
