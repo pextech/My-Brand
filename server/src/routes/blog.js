88fb306cac25df4b593f423b189d8eaa679b5e44
shemaM <azertshema@gmail.com>
2020-12-08 09:24:41 +0200

    adding asynchandler function to handler errors

--- server/src/routes/blog.js
+++ server/src/routes/blog.js
@@ -4,7 +4,10 @@ import asyncHandler from "../middlewares/async";
 
 const router = express.Router();
 
-router.route("/").post(asyncHandler(blog.create)).get(blog.getAll);
-router.route("/:id").get(blog.getOne);
+router
+  .route("/")
+  .post(asyncHandler(blog.create))
+  .get(asyncHandler(blog.getAll));
+router.route("/:id").get(asyncHandler(blog.getOne));
 
 export default router;
