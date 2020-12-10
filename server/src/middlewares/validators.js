090cf9b2f6a6fe45933135b2318120cc214845b0
shemaM <azertshema@gmail.com>
2020-12-10 19:49:11 +0200

    adding author in joi midllerware functions

--- server/src/middlewares/validators.js
+++ server/src/middlewares/validators.js
@@ -3,6 +3,7 @@ import { joiResponse } from "../util/joiresponse";
 
 export const blogValidator = (req, res, next) => {
   const schema = Joi.object({
+    Author: Joi.string().required(),
     Title: Joi.string().min(5).max(25).required(),
     Content: Joi.string().min(10).required(),
   });
